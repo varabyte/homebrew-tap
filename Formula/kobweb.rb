24b9cd5ac2a867a7c579e8c72cb83774e8760a40
David Herman <bitspittle@gmail.com>
2022-01-24 20:05:03 -0800

    kobweb cli-v0.9.2

--- Formula/kobweb.rb
+++ Formula/kobweb.rb
@@ -1,8 +1,8 @@
-# Generated with JReleaser 1.0.0-M1 at 2022-01-24T19:54:40.608492-08:00
+# Generated with JReleaser 1.0.0-M1 at 2022-01-24T20:05:01.303305-08:00
 class Kobweb < Formula
   desc "Kobweb CLI provides commands to handle the tedious parts of building a Compose for Web app"
   homepage "https://kobweb.varabyte.com/"
-  url "https://github.com/varabyte/kobweb/releases/download/cli-0.9.2/kobweb-0.9.2.zip"
+  url "https://github.com/varabyte/kobweb/releases/download/cli-v0.9.2/kobweb-0.9.2.zip"
   version "0.9.2"
   sha256 "38fd041427bffa943458cb80dc68425dc6dc5f7a0686386a3fbfe435c40d8f29"
   license "Apache-2.0"
