d2ac62bd2952f00d367141b54d7642cd5a91b8c2
David Herman <bitspittle@gmail.com>
2022-11-23 18:39:58 -0800

    kobweb cli-v0.9.10

--- Formula/kobweb.rb
+++ Formula/kobweb.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.0.0-M2 at 2022-04-09T23:07:26.43582832-07:00
+# Generated with JReleaser 1.3.1 at 2022-11-23T18:39:55.160518128-08:00
 class Kobweb < Formula
   desc "Set up and manage your Compose for Web app"
   homepage "https://kobweb.varabyte.com/"
-  url "https://github.com/varabyte/kobweb/releases/download/cli-v0.9.9/kobweb-0.9.9.zip"
-  version "0.9.9"
-  sha256 "a347b4c135d3213558d3158cbb0827e58a0fc607c1bca79952e16cb432138498"
+  url "https://github.com/varabyte/kobweb/releases/download/cli-v0.9.10/kobweb-0.9.10.zip"
+  version "0.9.10"
+  sha256 "34bb06e69914632c6d32ce13a566ca90a40049d062b255744b8e0a1dd4336635"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
@@ -16,6 +16,6 @@ class Kobweb < Formula
 
   test do
     output = shell_output("#{bin}/kobweb --version")
-    assert_match "0.9.9", output
+    assert_match "0.9.10", output
   end
 end
