d67f972ab7a9287b2134e4a2f217b355664ba6ba
David Herman <bitspittle@gmail.com>
2023-07-05 14:35:36 -0700

    kobweb v0.9.13

--- Formula/kobweb.rb
+++ Formula/kobweb.rb
@@ -1,8 +1,8 @@
-# Generated with JReleaser 1.7.0 at 2023-07-05T14:17:58.788433332-07:00
+# Generated with JReleaser 1.7.0 at 2023-07-05T14:35:31.683785536-07:00
 class Kobweb < Formula
   desc "Set up and manage your Compose HTML app"
   homepage "https://kobweb.varabyte.com/"
-  url "https://github.com/varabyte/kobweb-cli/releases/download/cli-v0.9.13/kobweb-0.9.13.zip"
+  url "https://github.com/varabyte/kobweb-cli/releases/download/v0.9.13/kobweb-0.9.13.zip"
   version "0.9.13"
   sha256 "a601a121324d520b50f8b04e8db46c9ffbd9db9a00d0c197bd0172571df97e19"
   license "Apache-2.0"
